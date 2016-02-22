a4f69959467d502d2ee8a90741d1555cf83aabb5
Chun Yang <yang2007chun@gmail.com>
2016-02-22 12:03:53 -0600

    bugfix: let user specify if he want to reload or noe

--- reload-prompt.js
+++ reload-prompt.js
@@ -2,6 +2,18 @@
 
 Tracker.autorun(function(){
   if (Reload.isWaitingForResume()){
+    // do not show prompt if the user set NO_RELOAD_PROMPT
+    let confirmWithoutPrompt = Meteor.settings &&
+      Meteor.settings.public &&
+      Meteor.settings.public.NO_RELOAD_PROMPT;
+
+    if (confirmWithoutPrompt) {
+      console.log( "Reload Prompt - Accepting update automatically due to settings" );
+      window.location.reload();
+      return
+    }
+
+    // prepare messages
     const appNameInSetting = Meteor.settings &&
       Meteor.settings.public &&
       Meteor.settings.public.APP_NAME;
@@ -9,24 +21,16 @@ Tracker.autorun(function(){
     const appName = appNameInSetting || 'this app';
 
     const message = `A new version of ${appName} is avaliable.`;
+
     if (Meteor.isCordova) {
       // for cordova
-
       const title = 'Update Available';
-      const confirmCallback = function() {
-        window.location.reload();
-      };
+      const confirmCallback = function() { window.location.reload(); };
       const buttonLabels = ['Update', 'Later'];
 
-      if (!Meteor.settings || !Meteor.settings.public || !Meteor.settings.public.NO_RELOAD_PROMPT) {
-        navigator.notification.confirm(message, confirmCallback, title, buttonLabels);
-      }else{
-        console.log( "Reload Prompt - Accepting update automatically due to settings" );
-        window.location.reload();
-      }
+      navigator.notification.confirm(message, confirmCallback, title, buttonLabels);
     } else {
       // browser
-
       const update = window.confirm(message);
 
       if (update) {
